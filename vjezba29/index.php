c84ca0a317d0442fa6851cb4ee21ff3dd2fd640a
Cecamax <57287400+Cecamax@users.noreply.github.com>
2019-12-10 21:07:42 +0100

    Add files via upload

---
+++ vjezba29/index.php
@@ -0,0 +1,45 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ucitavanje stranice</title>
+
+    <style>
+        body{ margin:0;}
+        .header{ width: 100%; }
+        ul{ background: #b93030; margin: 0; padding: 20px; text-align: center; }
+        li{ display: inline; }
+        a {text-decoration: none; color: #bc3434; background: #eee; padding:10px;}
+    </style>
+</head>
+<body>
+    <!--Ukljucivanje menija i dugmica-->
+    <?php require 'header.php'; ?>
+
+    <!-- Ucitavanje samih stranica-->
+    <?php 
+    if(isset($_GET ['page']) && !empty($_GET['page'])){
+        $page = $_GET['page'];
+
+        switch($page){
+            case 1:
+                require 'stranice/1.html';
+                break;
+            case 2:
+                require 'stranice/2.html';
+                break;
+            case 3:
+                require 'stranice/3.html';
+                break;
+            default:
+                require 'stranice/1.html';
+            break;
+        }
+    }else{
+        require 'stranice/1.html';
+    }
+
+    ?>
+</body>
+</html>
